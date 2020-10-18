91b248df5679703f3d23bce75a9c66bc83c61789
skylife <skylife.lei@outlook.com>
2020-10-18 16:09:11 +0800

    modify file postion

--- aoi_cpp/aoi/aoi/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-cd /data/home/user00/aoi
-rm -rf build/
-mkdir build
-cd build
-
-cmake ..
-make 
--- aoi_cpp/aoi/aoi/run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd /data/home/user00/aoi/build
-./Aoi $1 $2
-#gprof ./Aoi gmon.out|less -S
-
